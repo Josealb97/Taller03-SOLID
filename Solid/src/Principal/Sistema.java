ee041441da5c516c325ce5a9939c20be5d625562
Josealb97 <jame1197@hotmail.com>
2021-06-10 12:41:45 -0500

    Anadido y modificado codigo main para el funcionamiento del programa

--- Solid/src/Principal/Sistema.java
+++ Solid/src/Principal/Sistema.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 import Adicionales.Aderezo;
 import Adicionales.Crema;
@@ -19,9 +14,14 @@ public class Sistema {
      public static void main(String [ ] args){
         // Producir un helado de vainilla y una torta de chocolate, 
         // a ambos agregarles CREMA y FRUTILLAS
+        // y cambiar el tipo de leche por Leche Descremada
+        // Finalmente mostrar el precio final de cada uno
+        // Producir un helado de vainilla y una torta de chocolate, 
+        // a ambos agregarles CREMA y FRUTILLAS
         // y cambiar el tipo de leche por Leche Deslactosada
-        ArrayList<Postre> arrPostres = new ArrayList<>();
-        ManejadorDeLeche mnj_leche = new ManejadorDeLeche(new LecheDescremada());
+       
+         ArrayList<Postre> arrPostres = new ArrayList<>();
+        ManejadorDeLeche mnj_leche = new ManejadorDeLeche();
         
         // Producir Helado
         Postre helado_vainilla = new Helado("Vainilla");
@@ -30,13 +30,13 @@ public class Sistema {
         Postre pastel_chocolate = new Pastel("Chocolate");
         arrPostres.add(pastel_chocolate);
         
-        arrPostres.forEach(postre -> {
-            postre.addAderezos(new Crema());
-            postre.addAderezos(new Frutilla());
+        for(Postre postre : arrPostres){
+            postre.anadirAderezoPostre(new Crema());
+            postre.anadirAderezoPostre(new Frutilla());
             System.out.println(postre);
-            mnj_leche.cambiarTipoLeche(postre);
+            mnj_leche.cambiarTipoLeche(new LecheDescremada(),postre);
             System.out.println(ManejadorDePrecio.showPrecioFinal(postre));
-        });        
+        }  
         
     }
 
